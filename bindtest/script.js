b74d23b2f55f93519e25f1f49078004159a1220d
shinasho <shoyodo47@gmail.com>
2023-11-07 18:18:03 +0900

    bindtest done

---
+++ bindtest/script.js
@@ -0,0 +1,14 @@
+new Vue({
+  el: '#app',
+  data: {
+    fileName: 'face1.png',
+    myURL: 'https://www.ymori.com',
+    myAlign: 'right',
+    myColor: '#E0800',
+    mySize: '200%',
+    myBackColor: 'aqua',
+    myClass: 'strike-through',
+    darkClass: 'dark',
+    isON: true
+  }
+})
