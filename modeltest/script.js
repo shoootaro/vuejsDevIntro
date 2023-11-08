0c2119a97cf9bd54890b6f7d03acd19b01a31f26
shinasho <shoyodo47@gmail.com>
2023-11-08 23:38:04 +0900

    modeltest done

---
+++ modeltest/script.js
@@ -0,0 +1,16 @@
+new Vue({
+  el: '#app',
+  data: {
+    myName: '',
+    myText: '今日はいい天気でんがな',
+    myCheck: false,
+    myChecks: [],
+    myAgree: false,
+    picked: 'red',
+    fileName: '',
+    myColor: [],
+    myText2: '',
+    myNumber: '',
+    myText3: '',
+  }
+})
